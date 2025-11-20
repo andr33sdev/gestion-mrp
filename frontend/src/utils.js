513c2433d9cc58dd83b0f8d8dafbf9cecefaf9e6
andr33sdev <andresfcardoso1997@gmail.com>
2025-11-19 23:15:11 -0300

    analisys bug solved

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,6 +2,7 @@
 export const API_BASE_URL = "https://horno-backend.onrender.com/api";
 //export const API_BASE_URL = "http://localhost:4000/api";
 
+
 export const REGISTROS_API_URL = `${API_BASE_URL}/registros`;
 export const PRODUCCION_API_URL = `${API_BASE_URL}/produccion`;
 export const PEDIDOS_API_URL = `${API_BASE_URL}/pedidos-analisis`;
