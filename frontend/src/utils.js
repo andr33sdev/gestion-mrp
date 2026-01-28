8d442a47888cf3910d2509aaa6cc388df146e0cb
andr33sdev <andr33sdev@gmail.com>
2026-01-28 09:20:08 -0300

    3d iglu mobile version

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react"; // Esto probablemente sobre, utils 
 import { getAuthData, logout } from "./auth/authHelper";
 
 // --- CONSTANTES ---
-//export const API_BASE_URL = "https://horno-backend.onrender.com/api";
-export const API_BASE_URL = "http://localhost:4000/api";
+export const API_BASE_URL = "https://horno-backend.onrender.com/api";
+//export const API_BASE_URL = "http://localhost:4000/api";
 
 export const REGISTROS_API_URL = `${API_BASE_URL}/registros`;
 export const PRODUCCION_API_URL = `${API_BASE_URL}/produccion`;
